01ccdd7ab5d582b208f209794e75d67cc1a27bb4
barvin <arvin.behshad@gmail.com>
2014-03-16 00:17:34 +0100

    Completed assignment

--- ex44.py
+++ ex44.py
@@ -1,26 +1,50 @@
-"""
 
-
-#
-# IMPLICIT INHERITANCE
-#
 
 class Parent(object):
 
+	def override(self):
+		print "PARENT override()"
+		# override explicitly
+
 	def implicit(self):
 		print "PARENT implicit()"
+		# implicit inheritance
+
+	def altered(self):
+		print "PARENT altered()"
 
 class Child(Parent):
-	pass
+
+	def override(self):
+		print "CHILD override()"
+		# override explicitly
+
+	def altered(self):
+		print "CHILD, BEFORE PARENT altered()"
+		super(Child, self).altered()
+			# gets Parent.altered()
+		print "CHILD, AFTER PARENT altered()"
 
 dad = Parent()
 son = Child()
 
 dad.implicit()
+	#: PARENT implicit()
 son.implicit()
+	#: PARENT implicit()
 
+dad.override()
+	#: PARENT override()
+son.override()
+	#: CHILD override()
 
-"""
+dad.altered()
+	#: PARENT altered()
+son.altered()
+	#: CHILD, BEFORE PARENT altered()
+	#: PARENT altered()
+	#: CHILD, AFTER PARENT altered()
+
 
 """
 #
@@ -29,8 +53,7 @@ son.implicit()
 
 class Parent(object):
 
-	def override(self):
-		print "PARENT override()"
+	
 
 class Child(Parent):
 
@@ -43,7 +66,6 @@ son = Child()
 dad.override()
 son.override()
 
-"""
 
 #
 # ALTER BEFORE OR AFTER
@@ -66,3 +88,5 @@ son = Child()
 
 dad.altered()
 son.altered()
+
+"""
